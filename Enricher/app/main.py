645c84fe0615c52b670df4130f1918af47ce2737
kobi <kodigershuni@gmail.com>
2026-02-19 23:08:54 +0200

    last updates

--- Enricher/app/main.py
+++ Enricher/app/main.py
@@ -1,14 +1,48 @@
+import json
+
 from utils import *
 from consumer import get_data_from_kafka, consumer
 from mongo_connection import MongoService
+from redis_connection import ConnectRedis
 
 client = MongoService()
 client.connect()
 collection = client.create_collection()
+r = ConnectRedis()
+r.connect()
+
 
 consumer.subscribe(["cleaned-instructions"])
 
 print("🟢 Enricher consumer is running and subscribed to orders topic")
+
+def update_fields(data):
+    data = set_keys(data)
+    text = data["pizza_prep"]
+    analysis_data = read_from_file("data/pizza_analysis_lists.json")
+    lists = split_to_lists(analysis_data)
+    for current_list, key in zip(lists, keys):
+        changed = False
+        result = compatibility_check(current_list, text)
+        if result and not changed:
+            data[key] = not(data[key])
+            changed = True
+    if check_vegan_or_gluten(text, "VEGAN"):
+        data['vegan'] = True
+    if check_vegan_or_gluten(text, "GLUTEN-FREE"):
+        data['gluten_free'] = True
+    data = determine_kosher(data)
+    print(data)
+    r.insert(data["pizza_type"], json.dumps(data))
+    client.update({"order_id": data['order_id']}, {"$set": {
+            'is_alergan': data['is_alergan'], 
+            'is_kosher': data['is_kosher'],
+            'is_meat': data['is_meat'],
+            'is_dairy': data['is_dairy'],
+            'status': data['status'],
+            'pizza_prep': data['pizza_prep'],
+            'special_instructions': data['special_instructions']
+    }})
 
 def main():
     try:
@@ -17,31 +51,13 @@ def main():
             if not data: 
                 print("data is none")
                 continue
-            data = set_keys(data)
-            text = data["pizza_prep"]
-            analysis_data = read_from_file("data/pizza_analysis_lists.json")
-            lists = split_to_lists(analysis_data)
-            for current_list, key in zip(lists, keys):
-                changed = False
-                result = compatibility_check(current_list, text)
-                if result and not changed:
-                    data[key] = not(data[key])
-                    changed = True
-            if check_vegan_or_gluten(text, "VEGAN"):
-                data['vegan'] = True
-            if check_vegan_or_gluten(text, "GLUTEN-FREE"):
-                data['gluten_free'] = True
-            data = determine_kosher(data)
-            print(data)
-            client.update({"order_id": data['order_id']}, {"$set": {
-                    'is_alergan': data['is_alergan'], 
-                    'is_kosher': data['is_kosher'],
-                    'is_meat': data['is_meat'],
-                    'is_dairy': data['is_dairy'],
-                    'status': data['status'],
-                    'pizza_prep': data['pizza_prep'],
-                    'special_instructions': data['special_instructions']
-            }})
+            result = r.get_data(data['pizza_type'])
+            if result:
+                print("cahce hit")
+                print(json.loads(result))
+                continue
+            print("cache miss")
+            update_fields(data)
             print(f"finished updating keys for order: {data['order_id']} and sent updates to mongo")
     except KeyboardInterrupt:
         print("\n🔴 Stopping consumer")
---
+++ Enricher/app/redis_connection.py
@@ -0,0 +1,28 @@
+import redis
+from redis import RedisError
+import os
+import json
+
+
+host = os.getenv("REDIS_HOST")
+port = int(os.getenv("REDIS_PORT"))
+
+
+class ConnectRedis:
+    def __init__(self):
+        self.r = None
+
+    def connect(self):
+        self.r = redis.Redis(host=host, port=port, db=0)
+        return self.r
+    
+    def insert(self, key, value):
+        try:
+            self.r.set(key , value=json.dumps(value, default=str), ex=5)
+        except RedisError as e:
+            print("error inserting to redis")
+            raise e
+        
+    def get_data(self, key):
+        data = self.r.get(key)
+        return data
